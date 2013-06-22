8325c8d32355ff1670070429cc0f03cdd99db0af
Ignacy Moryc <imoryc@gmail.com>
2013-06-22 10:34:00 +0200

    Add some docs, cleanup some more

--- bayes.rb
+++ bayes.rb
@@ -5,33 +5,51 @@ class Array
 end
 
 class Bayes
-  attr_reader :dictionary, :vectors
+  attr_reader :dictionary, :vectors, :classes
 
-  def dictionary=(array)
-    @dictionary = array.flatten.uniq
+  # Creates new Bayesian clasiffier.
+  #
+  # Lets say that we have a vector of data extracted from emails that
+  # looks like this (this example of course if to small to build a workign
+  # clasiffier - to do that we would need many more emails):
+  #
+  # v = [
+  #   ["Dear", "John", "Thank", "you", "for", "the", "invitation"],
+  #   ["Hey", "you", "should", "buy", "viagra!!"]
+  # ]
+  #
+  # And we want to build a clasiffier that will filter out emails like the second
+  # one. To do that we can say that the first email is ok so has for example
+  # class = 1, and the second one is a spam and it's class is equal to 0.
+  #
+  # This would mean that *v*:: would be our vector and classes would be [1, 0]
+  # we would also need a dictionary of all possible words, which would be a set
+  # of all words in all emails, created by the code below from *v*::
+  #
+  # _Parameters_::
+  # +dictionary+:: array(s) of words that are treated by the clasiffier as valid
+  # +vector+:: list of N lists of text that we use to train the clasiffier
+  # +classes+:: list of N classes that correspond to postions in vector
+  def initialize(dictionary, vectors, classes)
+    @dictionary = dictionary.flatten.uniq
+    @vectors = vectors.each.inject([]) do |vs, post|
+      vs << words_in_the_dictionary(post)
+    end
+    @classes = classes
   end
 
   # Returns an array of 0 and 1. For every index of returned
   # array 0 means that the word at the same index of :words: array
   # is not present in the dictionary set and 1 means it is present.
   def words_in_the_dictionary(words)
-    matches = Array.new(@dictionary.size) { 0 } # initialize with 0
+    matches = Array.new(dictionary.size) { 0 } # initialize with 0
     words.each do |word|
-      matches[@dictionary.index(word)] = 1 if @dictionary.include?(word)
+      matches[dictionary.index(word)] = 1 if dictionary.include?(word)
     end
     matches
   end
 
-  # Given a set of texts creates a vector of 1 and 0 for
-  # each element of the set, where 1 means that given
-  # word is in dictionary and 0 means it's is not.
-  def vectors=(set)
-    @vectors = set.each.inject([]) do |vectors, post|
-      vectors << words_in_the_dictionary(post)
-    end
-  end
-
-  def train(classes)
+  def train
     p_that_belongs = classes.sum.to_f / classes.length
 
     p1, p0 = initialize_probabilities(vectors.first.size)
--- bayes_test.rb
+++ bayes_test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require_relative 'bayes'
 
 class BayesTest < Test::Unit::TestCase
-  def setup
+  def setup_full_test
     @dataSet = [
                 ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -11,22 +11,22 @@ class BayesTest < Test::Unit::TestCase
                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop','him'],
                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']
                ]
-    @classes = [0, 1, 0, 1, 0, 1] # 1 spam, 0 not spam
-    @bayes = Bayes.new
-    @bayes.dictionary = @dataSet
-    @bayes.vectors = @dataSet
-    @p0, @p1, @p_belongs =  @bayes.train(@classes)
+
+    @bayes = Bayes.new(@dataSet, @dataSet, [0, 1, 0, 1, 0, 1])
+    @p0, @p1, @p_belongs =  @bayes.train
   end
 
   def test_makes_unique_vocabulary
-    @bayes.dictionary = [["this", "is", "are"], ["this", "are", "plums"]]
-    assert_equal ["this", "is", "are", "plums"], @bayes.dictionary
+    dict = [["this", "is", "are"], ["this", "are", "plums"]]
+    bayes = Bayes.new dict, dict, []
+    assert_equal ["this", "is", "are", "plums"], bayes.dictionary
   end
 
   def test_matches_against_vocabulary
-    @bayes.dictionary = ["this", "this", "is", "something", "huge"]
+    dict = [["this", "this", "is", "something", "huge"]]
+    bayes = Bayes.new dict, dict, []
     text = ["this", "was", "a", "very", "huge", "deal"]
-    assert_equal [1, 0, 0, 1], @bayes.words_in_the_dictionary(text)
+    assert_equal [1, 0, 0, 1], bayes.words_in_the_dictionary(text)
   end
 
   def test_trains_naive_bayesian_classifier
@@ -34,21 +34,23 @@ class BayesTest < Test::Unit::TestCase
     post2 = ["offensive", "dick", "penis"]
     post3 = ["something", "penis", "other"]
     set = [post1, post2, post3]
-    @bayes.dictionary = set
-    classes = [0, 1, 1]
-    @bayes.vectors = set
-    p0, p1, p_belongs =  @bayes.train(classes)
+
+    bayes = Bayes.new(set, set, [0, 1, 1])
+    p0, p1, p_belongs =  bayes.train
     assert_equal 2.0/3, p_belongs
     assert_equal 8, p0.size
+    assert_equal [0.0, 0.0, 0.0, 0.125, 0.125, 0.25, 0.125, 0.125], p1
   end
 
   def test_classifying_real_data_spam
+    setup_full_test
     guesed = @bayes.classify(@bayes.words_in_the_dictionary(["stupid", "garbage"]),
                              @p0, @p1, @p_belongs)
     assert_equal 1, guesed
   end
 
   def test_classifying_real_data_non_spam
+    setup_full_test
     guesed = @bayes.classify(@bayes.words_in_the_dictionary(["love", "butterfly"]),
                              @p0, @p1, @p_belongs)
     assert_equal 0, guesed
